fd6add93d102b8faa096d9bf9a1c1fb7ad0f5f37
Alterra$Developer <42990792+AlterraDeveloper@users.noreply.github.com>
2024-08-15 19:11:17 +0600

    add lesson 1

---
+++ lesson_1/app.js
@@ -0,0 +1,76 @@
+// alert("Hello Group 41 FROM APP.JS");
+
+/*
+function getRandomColor() {
+    const colors = [
+        "red",
+        "yellow",
+        "green",
+        "purple",
+        "cyan",
+        "orange",
+        "purple",
+    ];
+    const colorIndex = Math.floor(Math.random() * colors.length);
+    return colors[colorIndex];
+}
+
+const btn = document.querySelector("button");
+
+btn.onclick = () => {
+  const color = getRandomColor();
+  btn.style.backgroundColor = color;
+};
+
+setInterval(() => {
+  const color = getRandomColor();
+  document.body.style.backgroundColor = color;
+}, 1000);
+*/
+
+// variables - переменные
+
+// var let const
+
+let myName = "Evgeniy";
+let mySurname = "Kiselev";
+let myFatherName = "Dmitrievich";
+
+console.log(mySurname + " " + myName + " " + myFatherName);
+
+let myAge = 18;
+console.log("In next year my age will be " + (myAge + 1));
+
+// Data types - типы данных
+
+// 1. string - текстовый
+// 2. number - числовой
+// 3. boolean - логический
+// 4. undefined - неопределенный
+// 5. null - пустой
+// 6. object - объект
+// 7. bigint
+// 8. Symbol
+
+let producerName = "Xiaomi";
+let modelName = 'Redmi 14S MAX';
+
+console.log("My smartphone is:" + producerName + " " + modelName);
+
+let cpuCores = 4;
+let ram = 8;
+let storage = 128;
+let price = 25_000_000;
+let bluetouthVersion = 5.2;
+
+console.log("Smartphone sale price (50%):" + price / 2);
+
+let supportsNFC = false; // 0
+let supports3G = true; // 1
+let supports4G = true;
+let supports5G = false;
+
+
+
+
+
